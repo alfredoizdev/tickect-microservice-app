5286b164e5eeaef057ce4915acac04c301a78493
Alfredo Izquierdo <alfredoizquierdo@Alfredos-MacBook-Pro-2.local>
2024-07-15 20:06:13 -0500

    add configmap

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,8 @@ import mongoose from "mongoose";
 import { app } from "./app";
 
 const start = async () => {
+  console.log("Starting up...", process.env.MONGO_URI);
+
   if (!process.env.MONGO_URI) {
     throw new Error("MONGO_URI must present");
   }
